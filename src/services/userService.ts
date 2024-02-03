69fb6bd83b45bfe4513e7bd507ce2f53a98dd60c
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2024-02-03 11:21:32 -0300

    chore: add signin page with request

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,10 +1,15 @@
-import { SignupData } from "@/@types/user";
+import { SigninData, SignupData } from "@/@types/user";
 import axiosInstance from "@/config/axios.config";
 
 async function signup(body: SignupData) {
   return axiosInstance.post("/user/signup", body);
 }
 
+async function signin(body: SigninData) {
+  return axiosInstance.post("/user/signin", body);
+}
+
 export const userService = {
   signup,
+  signin
 };
