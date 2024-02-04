96848d4835ad6849d9124717024a7c63af153a31
Afonso Cardozo Cruz <afonso.cardozo.cruz@gmail.com>
2024-02-04 18:51:17 -0300

    feat: add header component

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "../globals.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
+import Header from "@/components/ui/Header/Header";
+import { Container } from "@mui/material";
 
 export const metadata: Metadata = {
   title: "Orange Portfólio",
@@ -18,15 +20,18 @@ export default function RootLayout({
     <html lang="pt-br">
       <CssBaseline />
       <body>
-        <Box
-          component={"main"}
-          maxWidth={"100%"}
-          height="100%"
-          display={"flex"}
-          flexDirection={"column"}
-        >
-          {children}
-        </Box>
+        <Header />
+        <Container maxWidth={"lg"}>
+          <Box
+            component={"main"}
+            maxWidth={"100%"}
+            height="100%"
+            display={"flex"}
+            flexDirection={"column"}
+          >
+            {children}
+          </Box>
+        </Container>
       </body>
     </html>
   );
