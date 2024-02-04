27500ad77ac585bc0de63edb56a04be006b1dc64
Pedro <pedrocastillo24.pc@gmail.com>
2024-02-04 14:38:30 -0300

    create layout discover mobile

--- src/app/allProjects/page.tsx
+++ src/app/allProjects/page.tsx
@@ -30,7 +30,7 @@ export default function page() {
 
       const response = await fetch('http://localhost:3001/project/all');
       const data = await response.json();
-  
+
         return setState(data)
         
       
@@ -47,6 +47,38 @@ export default function page() {
         font-sans font-normal text-2xl/[24px] text-center m-auto" >
         Junte-se à comunidade de inovação, inspiração e descobertas, 
         transformando experiências em conexões inesquecíveis
+        </div>
+
+        <div  >
+          {state.map((t,i) => (
+            <Card key={t.id} className=" w-[312px] h-[298px] mt-[30px] m-auto" >
+
+            <img  src={t.url_image} alt="image of project" className="w-[100%] h-[80%]" />
+
+            <div className="flex justify-between w-[100%] h-[40px] mt-[10px]  " >
+
+              <div className="  w-[190px] h-[32px] flex justify-between " >
+
+                <div className="mt-[7px]" >
+                  <img  src={t.url_image} alt="image of project" className="w-[24px] h-[24px] rounded-[100%] " />
+                </div>
+
+                   
+                 <div className="flex w-[158px] h-[16px] font-sans font-normal text-base  m-auto  " > 
+                  <div className="w-[100%] text-center " >Pedro Castillo - </div>
+                    {(i + 1) +"/"+state.length}
+                    </div> 
+              </div>
+
+              <div  className="  w-[94px] h-[32px] flex justify-end  "> 
+                <div className="w-[46px] h-[32px] bg-silver-400 bg-slate-400 rounded-[100px] text-center  flex align-center  p-[4px]" >
+                 <div className="w-[38px] h-[24px] mt-auto " >{t.ProjectTag.map(e => e.Tag.name)}</div> 
+                  </div>
+               </div>
+            </div>
+            
+            </Card>
+          ))}
         </div>
     </>
   )
